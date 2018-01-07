7059995118632d7173c202d7de657f5c6e32f454
Cracked679 <31470255+Cracked679@users.noreply.github.com>
2018-01-07 20:57:37 +0530

    Moved data from view into the controller on all other pages.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -31,10 +31,58 @@ module.exports.homelist = function(req, res){
 
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res){
-  res.render('location-info', { title: 'Location info' });
+  res.render('location-info', { 
+    title: 'Starcups',
+    pageHeader:{title: 'Starcups'},
+    sidebar:{
+      context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+      callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+    },
+    location:{
+      name:'Starcups',
+      address:'125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      facilities:['Hotdrinks','Food','PremiumWifi'],
+      openingTimes: [{
+        days: 'Monday - Friday',
+        opening: '7:00am',
+        closing: '7:00pm',
+        closed: false
+      }, {
+        days: 'Saturday',
+        opening: '8:00am',
+        closing: '5:00pm',
+        closed: false
+      }, {
+        days: 'Sunday',
+        closed: true
+      }],
+      reviews: [{
+        author: 'Simon Holmes',
+        rating: 5,
+        timestamp: '16 July 2013',
+        reviewText: 'What a great place. I can\'t say enough good things about it.'
+        }, 
+        {
+        author: 'Charlie Chaplin',
+        rating: 3,
+        timestamp: '16 June 2013',
+        reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+        }],
+      coords:{
+        lat: 19.975136, 
+        lng: 73.752747
+      }
+    }
+  });
 };
 
 /* GET 'Add review' page */
 module.exports.addReview = function(req, res){
-  res.render('location-review-form', { title: 'Add review' });
+  res.render('location-review-form', { 
+    title: 'Review Starcups on Loc8r',
+    pageHeader: {
+      title: 'Review Starcups' 
+    }
+   });
 };
